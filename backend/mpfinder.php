9e8fc8ab39d264c29b0531597af3c3e0d0114dc4
fonorobert <fonorobert@gmail.com>
2014-07-14 01:52:15 +0200

    start building csv parser

--- backend/mpfinder.php
+++ backend/mpfinder.php
@@ -2,9 +2,29 @@
 
 require_once 'database.php';
 
-$db = new CRUD();
 
-$db->close();
+
+$filename = '../resources/oevk/Bacs-Kiskun_korzet.csv';
+
+$data = array();
+ if ($handle = fopen($filename))
+    {
+        while (($row = fgetcsv($handle, 1000, $delimiter)) !== FALSE)
+        {
+            if(!$header)
+                $header = $row;
+            else
+                $data[] = array_combine($header, $row);
+        }
+        fclose($handle);
+        var_dump($data);
+    } else 
+    {
+    	echo 'shit';
+    }
+
+
+
 
 
 ?>
